965f0242e267db461361260e504739119e99e4e4
DevJonathanMendes <jonathanbarreiro88@gmail.com>
2023-02-18 15:23:39 -0300

    Improvements className

--- client/src/components/notes/Notes.tsx
+++ client/src/components/notes/Notes.tsx
@@ -2,55 +2,68 @@ import "./notes.css";
 
 const Notes = () => {
     const notes = [];
+
     for (let i = 0; i < 12; i++) {
-        notes.push(<li>
-            <p className="text">
-                Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-                Soluta atque doloremque excepturi iste enim aliquam
-                quaerat quidem perferendis? Dolorum quis doloribus
-                provident earum deleniti facere sint! Voluptates
-                voluptatum earum aut?
-            </p>
-        </li>);
+        notes.push(
+            <li className="notes-list-item">
+                <p className="notes-list-item-text">
+                    Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                    Soluta atque doloremque excepturi iste enim aliquam
+                    quaerat quidem perferendis? Dolorum quis doloribus
+                    provident earum deleniti facere sint! Voluptates
+                    voluptatum earum aut?
+                </p>
+            </li>
+        );
     };
 
     return (
         <div className="notes">
-            <div className="ulist">
+            <div className="notes-list">
                 <header>
                     <h2>Notes</h2>
                     <input type="search" placeholder="Research..." />
                 </header>
-                <div className="ulist2">
+
+                <div className="notes-list-container">
                     <ul>{notes}</ul>
                 </div>
+
             </div>
-            <div className="texts">
-                <p className="currentText">
+            <div className="note">
+                <p className="note-text">
                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis
                     veritatis voluptate fugit numquam. Obcaecati reprehenderit, harum
                     quas sapiente optio iste ducimus consequatur eos ex impedit eaque
                     quisquam exercitationem officiis voluptatem!
 
-                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Commodi dolorem aliquid minima fuga. Ad odio qui, necessitatibus impedit vero nobis vitae autem quaerat, soluta aspernatur nihil, culpa ipsa eum porro.
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Commodi
+                    dolorem aliquid minima fuga. Ad odio qui, necessitatibus impedit
+                    vero nobis vitae autem quaerat, soluta aspernatur nihil, culpa ipsa eum porro.
                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis
                     veritatis voluptate fugit numquam. Obcaecati reprehenderit, harum
                     quas sapiente optio iste ducimus consequatur eos ex impedit eaque
                     quisquam exercitationem officiis voluptatem!
 
-                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Commodi dolorem aliquid minima fuga. Ad odio qui, necessitatibus impedit vero nobis vitae autem quaerat, soluta aspernatur nihil, culpa ipsa eum porro.
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Commodi
+                    dolorem aliquid minima fuga. Ad odio qui, necessitatibus impedit
+                    vero nobis vitae autem quaerat, soluta aspernatur nihil, culpa ipsa eum porro.
                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis
                     veritatis voluptate fugit numquam. Obcaecati reprehenderit, harum
                     quas sapiente optio iste ducimus consequatur eos ex impedit eaque
                     quisquam exercitationem officiis voluptatem!
 
-                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Commodi dolorem aliquid minima fuga. Ad odio qui, necessitatibus impedit vero nobis vitae autem quaerat, soluta aspernatur nihil, culpa ipsa eum porro.
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Commodi
+                    dolorem aliquid minima fuga. Ad odio qui, necessitatibus impedit
+                    vero nobis vitae autem quaerat, soluta aspernatur nihil, culpa ipsa eum porro.
                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis
                     veritatis voluptate fugit numquam. Obcaecati reprehenderit, harum
                     quas sapiente optio iste ducimus consequatur eos ex impedit eaque
                     quisquam exercitationem officiis voluptatem!
 
-                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Commodi dolorem aliquid minima fuga. Ad odio qui, necessitatibus impedit vero nobis vitae autem quaerat, soluta aspernatur nihil, culpa ipsa eum porro.
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Commodi
+                    dolorem aliquid minima fuga. Ad odio qui, necessitatibus impedit
+                    vero nobis vitae autem quaerat, soluta aspernatur nihil, culpa ipsa eum porro.
                 </p>
             </div>
         </div >
