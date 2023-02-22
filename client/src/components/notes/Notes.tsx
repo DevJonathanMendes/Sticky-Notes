854c125a250b31116a71fbc6a082e278c468dc48
DevJonathanMendes <jonathanbarreiro88@gmail.com>
2023-02-22 15:05:14 -0300

    NoteHeader component created

--- client/src/components/notes/Notes.tsx
+++ client/src/components/notes/Notes.tsx
@@ -3,43 +3,56 @@ import "./notes.css";
 
 const stickyNotesMock = [{
     id: "a0",
-    text: "Sticky Note 0"
+    text: "Sticky Note 0",
+    selected: false
 }, {
     id: "a1",
-    text: "Sticky Note 1"
+    text: "Sticky Note 1",
+    selected: false
 }, {
     id: "a3",
-    text: "Sticky Note 2"
+    text: "Sticky Note 2",
+    selected: false
 }, {
     id: "a4",
-    text: "Sticky Note 3"
+    text: "Sticky Note 3",
+    selected: false
 }];
+
+const NotesHeader = () => {
+    return (
+        <header className="notes-list-header">
+            <h2>Sticky Notes</h2>
+            <input
+                type="search"
+                className="notes-list-header-input"
+                placeholder="Research..."
+            />
+        </header>
+    );
+};
 
 const Notes = () => {
     const [notes, setNotes] = useState(stickyNotesMock);
-    const [noteSelected, setNoteSelected] = useState("a0");
 
     return (
         <div className="notes">
             <div className="notes-list">
-                <header className="notes-list-header">
-                    <h2>Sticky Notes</h2>
-                    <input className="notes-list-header-input" type="search" placeholder="Research..." />
-                </header>
-
+                <NotesHeader />
                 <div className="notes-list-container">
-                    <ul>{notes.map(({ id, text }) =>
-                        <li key={id} className="notes-list-item" onClick={() => setNoteSelected(id)}>
-                            <p className="notes-list-item-text">{text}</p>
-                        </li>
-                    )}</ul>
+                    <ul>{
+                        notes.map(({ id, text }) =>
+                            <li key={id} className="notes-list-item">
+                                <p className="notes-list-item-text">{text}</p>
+                            </li>)
+                    }</ul>
                 </div>
             </div>
-            <div className="note">{
-                notes.map(({ id, text }) => id === noteSelected
-                    ? <div className="note-text"><textarea>{text}</textarea></div>
-                    : null)
-            }</div>
+            <div className="note">
+                <div className="note-text">
+                    <textarea defaultValue="A selected note." />
+                </div>
+            </div>
         </div >
     );
 };
