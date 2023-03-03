26dd2c81b5661f3c5f705c5eaf37c41cb1ca1ae5
DevJonathanMendes <jonathanbarreiro88@gmail.com>
2023-03-03 14:18:32 -0300

    style: Refactor

--- client/src/components/notes/NotesSelected/NotesSelected.tsx
+++ client/src/components/notes/NotesSelected/NotesSelected.tsx
@@ -1,3 +1,5 @@
+import "./NotesSelected.css";
+
 const NoteSelected = (props) => {
     const note = props.notes.filter(note => note.selected)[0];
 
@@ -6,7 +8,7 @@ const NoteSelected = (props) => {
 
     return note ?
         <div key={note.id} className="note">
-            <div className={`note-text  ${note.color}`}>
+            <div className={`${note.color}`}>
                 <textarea onChange={handleChange} placeholder="Write a note..." value={note.text} />
             </div>
         </div> : null;
