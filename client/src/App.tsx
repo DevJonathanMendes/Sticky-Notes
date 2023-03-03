31acd92371f223cbb56571f6b41729b21a0dfaf6
DevJonathanMendes <jonathanbarreiro88@gmail.com>
2023-03-02 21:56:33 -0300

    fix and feat

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,19 @@
-import "./app.css";
+import { useState } from "react";
 import Notes from "./components/notes/Notes";
 import Todo from "./components/todo/Todo";
+import "./app.css";
 
 const App = () => {
+  const [mode, setMode] = useState(true);
+
   return (
-    <>
-      <Todo />
-    </>
+    <div className="app-container">
+      <div>
+        <span onClick={() => setMode(true)}>NOTE</span>
+        <span onClick={() => setMode(false)}>NOTE</span>
+      </div>
+      {mode ? <Notes /> : <Todo />}
+    </div>
   );
 };
 
