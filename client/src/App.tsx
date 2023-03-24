352ad20c8398bb3a59d4abce91d7726c7d9663c5
DevJonathanMendes <jonathanbarreiro88@gmail.com>
2023-03-24 18:22:27 -0300

    Improvements switch app

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import Notes from "./components/notes/Notes";
 import Todo from "./components/todo/Todo";
+
 import "./app.css";
 
 const App = () => {
@@ -8,10 +9,24 @@ const App = () => {
 
   return (
     <div className="app-container">
-      <span className="switch">
-        <div onClick={() => setMode(true)}>NOTE</div>
-        <div onClick={() => setMode(false)}>NOTE</div>
-      </span>
+      <div className="switch">
+
+        <span onClick={() => setMode(true)}>
+          <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill={mode ? "currentColor" : "grey"} viewBox="0 0 16 16">
+            <path d="M1.5 0A1.5 1.5 0 0 0 0 1.5V13a1 1 0 0 0 1 1V1.5a.5.5 0 0 1 .5-.5H14a1 1 0 0 0-1-1H1.5z" />
+            <path d="M3.5 2A1.5 1.5 0 0 0 2 3.5v11A1.5 1.5 0 0 0 3.5 16h6.086a1.5 1.5 0 0 0 1.06-.44l4.915-4.914A1.5 1.5 0 0 0 16 9.586V3.5A1.5 1.5 0 0 0 14.5 2h-11zM3 3.5a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 .5.5V9h-4.5A1.5 1.5 0 0 0 9 10.5V15H3.5a.5.5 0 0 1-.5-.5v-11zm7 11.293V10.5a.5.5 0 0 1 .5-.5h4.293L10 14.793z" />
+          </svg>
+        </span>
+
+        <span onClick={() => setMode(false)}>
+          <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill={mode ? "grey" : "currentColor"} viewBox="0 0 16 16">
+            <path fill-rule="evenodd" d="M2 2.5a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h1a.5.5 0 0 0 .5-.5V3a.5.5 0 0 0-.5-.5H2zM3 3H2v1h1V3z" />
+            <path d="M5 3.5a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5zM5.5 7a.5.5 0 0 0 0 1h9a.5.5 0 0 0 0-1h-9zm0 4a.5.5 0 0 0 0 1h9a.5.5 0 0 0 0-1h-9z" />
+            <path fill-rule="evenodd" d="M1.5 7a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5H2a.5.5 0 0 1-.5-.5V7zM2 7h1v1H2V7zm0 3.5a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h1a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5H2zm1 .5H2v1h1v-1z" />
+          </svg>
+        </span>
+
+      </div>
       {mode ? <Notes /> : <Todo />}
     </div>
   );
--- client/src/components/todo/TodoForm/TodoForm.tsx
+++ client/src/components/todo/TodoForm/TodoForm.tsx
@@ -38,7 +38,11 @@ const TodoForm = (props: IPropsTodoForm) => {
         <form className="todo-form" onSubmit={handleSubmit}>
             {props.upTodo ? <>
                 <div className="todo-form-input">
-                    <input type="text" onChange={handleChange} value={inputTextValue} placeholder="Edit To-Do" />
+                    <input type="text"
+                        onChange={handleChange}
+                        value={inputTextValue}
+                        placeholder="Edit To-Do"
+                    />
                     <button type="submit">
                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 16 16">
                             <path d="M12.736 3.97a.733.733 0 0 1 1.047 0c.286.289.29.756.01 1.05L7.88 12.01a.733.733 0 0 1-1.065.02L3.217 8.384a.757.757 0 0 1 0-1.06.733.733 0 0 1 1.047 0l3.052 3.093 5.4-6.425a.247.247 0 0 1 .02-.022Z" />
@@ -47,7 +51,11 @@ const TodoForm = (props: IPropsTodoForm) => {
                 </div>
             </> : <>
                 <div className="todo-form-input">
-                    <input type="text" onChange={handleChange} value={inputTextValue} placeholder="Add To-Do" />
+                    <input type="text"
+                        onChange={handleChange}
+                        value={inputTextValue}
+                        placeholder="Add To-Do"
+                    />
                     <button type="submit">
                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 16 16">
                             <path fillRule="evenodd" d="M8 2a.5.5 0 0 1 .5.5v5h5a.5.5 0 0 1 0 1h-5v5a.5.5 0 0 1-1 0v-5h-5a.5.5 0 0 1 0-1h5v-5A.5.5 0 0 1 8 2Z" />
