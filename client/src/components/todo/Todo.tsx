1133d12caddc451aa4c5c56a6eb45fe268736f4c
DevJonathanMendes <jonathanbarreiro88@gmail.com>
2023-03-25 18:29:50 -0300

    feat: Add localStorage

--- client/src/components/todo/Todo.tsx
+++ client/src/components/todo/Todo.tsx
@@ -5,20 +5,37 @@ import TodoList from './TodoList/TodoList';
 
 import './Todo.css';
 
+const localTodos: ITodo[] = [];
+
+for (let indexItem = 0; indexItem < localStorage.length; indexItem++) {
+    const keyNote: string = localStorage.key(indexItem) || "";
+    const itemTodoStr = localStorage.getItem(keyNote) || "";
+    const itemTodoObj = JSON.parse(itemTodoStr);
+
+    if (itemTodoObj.type === "todo")
+        localTodos.push(itemTodoObj);
+
+};
+
 const Todo = () => {
-    const [todos, setTodos] = useState<ITodo[]>([]);
+    const [todos, setTodos] = useState<ITodo[]>(localTodos);
 
     const createTodo = (todo: ITodo) => {
         if (!todo.text) return;
 
+        localStorage.setItem(todo.id, JSON.stringify(todo));
         setTodos([todo, ...todos]);
     };
 
-    const updateTodo = (upTodo: ITodo) =>
+    const updateTodo = (upTodo: ITodo) => {
+        localStorage.setItem(upTodo.id, JSON.stringify(upTodo));
         setTodos(todos.map(todo => todo.id === upTodo.id ? upTodo : todo));
+    };
 
-    const deleteTodo = (todoId: string) =>
+    const deleteTodo = (todoId: string) => {
+        localStorage.removeItem(todoId);
         setTodos(todos.filter(({ id }) => id !== todoId));
+    };
 
     return (
         <div className='todo'>
