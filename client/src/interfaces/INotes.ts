7f6908383030788afeb63af1b7ad4a59458070cf
DevJonathanMendes <jonathanbarreiro88@gmail.com>
2023-04-08 18:51:37 -0300

    Irrelevant

--- client/src/interfaces/INotes.ts
+++ client/src/interfaces/INotes.ts
@@ -14,18 +14,18 @@ interface IPropsNoteHeader {
     readNote: (id: string) => void,
     deleteNote: (id: string) => void,
     searchNote: (eventInput: string) => void
-}
+};
 
 interface IPropsNoteList {
     notes: INote[],
     readNote: (id: string) => void,
     deleteNote: (id: string) => void
-}
+};
 
 interface IPropsNoteSelected {
     notes: INote[],
     updateNote: (id: string, text: string) => void,
     setNewColor: (color: string, id?: string) => void
-}
+};
 
 export type { INote, IPropsNoteHeader, IPropsNoteList, IPropsNoteSelected };
