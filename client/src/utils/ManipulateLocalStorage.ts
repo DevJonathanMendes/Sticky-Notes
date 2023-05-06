62f6c8040370e7b32c97cc387338667a8834c809
DevJonathanMendes <jonathanbarreiro88@gmail.com>
2023-05-06 06:56:05 -0300

    Updated readability

--- client/src/utils/ManipulateLocalStorage.ts
+++ client/src/utils/ManipulateLocalStorage.ts
@@ -6,13 +6,14 @@ const setItem = (id: string, obj: {}) => {
 
 const getItem = (id: string) => {
     const items = localStorage.getItem(id);
+
     return items ? JSON.parse(items) : null;
 };
 
 class ManipulateLocalStorage {
     constructor(key: string) {
         this.keyName = key;
-        getItem(key) || setItem(this.keyName, []);
+        getItem(key) || setItem(key, []);
     };
 
     private keyName: string;
@@ -20,11 +21,13 @@ class ManipulateLocalStorage {
     createItem(newItem: {}) {
         const items = this.readItem();
         items.unshift(newItem);
+
         setItem(this.keyName, items);
     };
 
     readItem(id?: string) {
         const items = getItem(this.keyName);
+
         return id
             ? items.filter((item: Id) => item.id === id)
             : items;
@@ -32,12 +35,14 @@ class ManipulateLocalStorage {
 
     updateItem(upItem: Id) {
         const items = this.readItem();
+
         setItem(this.keyName, items.map((item: Id) =>
             item.id === upItem.id ? upItem : item));
     };
 
     deleteItem(id: string) {
         const items = this.readItem();
+
         setItem(this.keyName, items.filter((item: Id) =>
             item.id != id));
     };
