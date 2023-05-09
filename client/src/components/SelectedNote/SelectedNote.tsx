e5116f3018a57ed4796556eacbfbc67403014622
DevJonathanMendes <jonathanbarreiro88@gmail.com>
2023-05-09 09:51:32 -0300

    Add useEffect => .focus

--- client/src/components/SelectedNote/SelectedNote.tsx
+++ client/src/components/SelectedNote/SelectedNote.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useRef } from "react";
 import { ISelectedNote } from "../../interfaces/ISelectedNote";
 import Colors from "../Colors/Colors";
 
@@ -9,6 +10,12 @@ const NoteSelected: ISelectedNote = (props) => {
     const handleChange = (e: React.ChangeEvent<HTMLTextAreaElement>) =>
         props.updateNote(note.id, e.target.value);
 
+    const textareaRef = useRef(null);
+
+    useEffect(() => {
+        note && textareaRef.current.focus();
+    });
+
     return note ?
         <div className="note">
             <div className={`container-colors ${note.color}`}>
@@ -19,6 +26,7 @@ const NoteSelected: ISelectedNote = (props) => {
             </div>
             <div className="container-textarea">
                 <textarea onChange={handleChange}
+                    ref={textareaRef}
                     placeholder="Write a note..."
                     value={note.text}
                 />
